25858fc5521f07d43690c52ccc103c22287a6a97
MIckey101987 <emhdev2019@gmail.com>
2021-06-01 12:31:53 +0200

    Adding new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { AboutComponent } from './components/about/about.component';
+import { StatsComponent } from './components/stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    WelcomeComponent,
+    BrandsComponent,
+    FeaturesComponent,
+    AboutComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule
